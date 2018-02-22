90321afe06ddbaec2f43b650a4d76e9246adba8f
GustavoASilveira <gustavo.nordheim@gmail.com>
2018-02-22 14:04:18 -0300

    tentando ajustar tela 5

--- js/boot.js
+++ js/boot.js
@@ -6,8 +6,10 @@ var bootState = {
 	create: function(){
 		game.scale.pageAlignHorizontally = true;
 		game.scale.pageAlignVertically = true;
+		game.scale.maxWidth = window.innerWidth;
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		game.disableVisibilityChange = true;
+		
 	
 		game.state.start('load');
 	}
