6d68c96cc69724770338110dc16e16a05c6c2ca5
GustavoASilveira <gustavo.nordheim@gmail.com>
2018-02-22 11:21:21 -0300

    tentando ajustar tela 2

--- js/boot.js
+++ js/boot.js
@@ -4,6 +4,10 @@ var bootState = {
 	},
 	
 	create: function(){
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+	
 		game.state.start('load');
 	}
 };
